b89d3d1badf49b241186e0a36fde45c3511cd6a5
SANKALPA HOTA <62276478+Sankalpa-Hota@users.noreply.github.com>
2025-11-15 03:28:46 +0530

    Update reduce_mod_poly1305.v

--- rtl/reduce_mod_poly1305.v
+++ rtl/reduce_mod_poly1305.v
@@ -1,4 +1,6 @@
 `timescale 1ns/1ps
+`default_nettype none
+
 module reduce_mod_poly1305(
     input wire clk,
     input wire reset_n,
@@ -12,25 +14,23 @@ module reduce_mod_poly1305(
     reg [129:0] lo;
     reg [127:0] hi;
     reg [130:0] tmp;
-    reg state;
+    reg running;
 
     always @(posedge clk or negedge reset_n) begin
         if(!reset_n) begin
-            value_out <= 130'b0;
-            busy <= 1'b0;
-            done <= 1'b0;
-            val_reg <= 258'b0;
-            state <= 1'b0;
-            lo <= 0;
-            hi <= 0;
-            tmp <= 0;
+            val_reg <= 0;
+            value_out <= 0;
+            busy <= 0;
+            done <= 0;
+            running <= 0;
         end else begin
-            done <= 1'b0;
-            if(start && !busy) begin
-                busy <= 1'b1;
+            done <= 0; // pulse
+
+            if(start && !running) begin
                 val_reg <= value_in;
-                state <= 1'b1;
-            end else if(busy && state) begin
+                busy <= 1;
+                running <= 1;
+            end else if(running) begin
                 lo = val_reg[129:0];
                 hi = val_reg[257:130];
                 tmp = lo + (hi * 5);
@@ -38,9 +38,10 @@ module reduce_mod_poly1305(
                     value_out <= tmp - (1'b1 << 130) + 5;
                 else
                     value_out <= tmp[129:0];
-                busy <= 1'b0;
-                done <= 1'b1;
-                state <= 1'b0;
+
+                busy <= 0;
+                done <= 1;   // single-cycle pulse
+                running <= 0;
             end
         end
     end
